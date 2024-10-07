c9b614aaae1f46040873de1ac6ce24a776441b81
danish0511 <dsdd0511@gmail.com>
2024-10-07 23:48:35 +0530

    first commit

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+// export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
+//     landingPageMenu: LANDING_PAGE_MENU,
+//     signUpForm: SIGN_UP_FORM,
+//     signInForm: SIGN_IN_FORM,
+//     groupList: GROUP_LIST,
+//     createGroupPlaceholder: CREATE_GROUP_PLACEHOLDER,
+//     groupPageMenu: GROUP_PAGE_MENU,
+// }
