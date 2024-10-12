87c6359889b849f99a612366e950eb17249aa553
danish0511 <dsdd0511@gmail.com>
2024-10-12 10:50:05 +0530

    landing page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,19 @@
-// export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
-//     landingPageMenu: LANDING_PAGE_MENU,
+import { LANDING_PAGE_MENU, MenuProps } from "./menus";
+
+type GroupleConstantsProps = {
+    landingPageMenu: MenuProps[]
+    // signUpForm: AuthFormProps[]
+    // signInForm: AuthFormProps[]
+    // groupList: GroupListProps[]
+    // createGroupPlaceholder: CreateGroupPlaceholderProps[]
+}
+
+
+export const GROUPLE_CONSTANTS: GroupleConstantsProps = {
+    landingPageMenu: LANDING_PAGE_MENU,
 //     signUpForm: SIGN_UP_FORM,
 //     signInForm: SIGN_IN_FORM,
 //     groupList: GROUP_LIST,
 //     createGroupPlaceholder: CREATE_GROUP_PLACEHOLDER,
 //     groupPageMenu: GROUP_PAGE_MENU,
-// }
+}
--- src/constants/menus.tsx
+++ src/constants/menus.tsx
@@ -1,3 +1,5 @@
+import { CreditCard, Explore, Home } from "@/icons"
+
 export type MenuProps = {
     id: number
     label: string
@@ -7,25 +9,25 @@ export type MenuProps = {
     integration?: boolean
 }
 
-// export const LANDING_PAGE_MENU: MenuProps[] = [
-//     {
-//         id: 0,
-//         label: "Home",
-//         icon: <Home />,
-//         path: "/",
-//         section: true,
-//     },
-//     {
-//         id: 1,
-//         label: "Pricing",
-//         icon: <CreditCard />,
-//         path: "#pricing",
-//         section: true,
-//     },
-//     {
-//         id: 1,
-//         label: "Explore",
-//         icon: <Explore />,
-//         path: "/explore",
-//     },
-// ]
+export const LANDING_PAGE_MENU: MenuProps[] = [
+    {
+        id: 0,
+        label: "Home",
+        icon: <Home />,
+        path: "/",
+        section: true,
+    },
+    {
+        id: 1,
+        label: "Pricing",
+        icon: <CreditCard />,
+        path: "#pricing",
+        section: true,
+    },
+    {
+        id: 1,
+        label: "Explore",
+        icon: <Explore />,
+        path: "/explore",
+    },
+]
