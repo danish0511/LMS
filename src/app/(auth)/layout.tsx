a88d43a33f49799f9938cf59d0d451be44431f26
danish0511 <dsdd0511@gmail.com>
2024-10-14 09:15:09 +0530

    prisma custom

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const AuthLayout = async ({ children }: Props) => {
+    const user = await onAuthenticatedUser()
+  return (
+    <div>AuthLayout</div>
+  )
+}
+
+export default AuthLayout
