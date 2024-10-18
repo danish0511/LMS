8d964060679e06b0d1a090213d449c0430e5663b
danish0511 <dsdd0511@gmail.com>
2024-10-18 15:12:25 +0530

    sign-in auth

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,6 +1,6 @@
 "use server"
 
-// import { client } from "@/lib/prisma"
+import { client } from "@/lib/prisma"
 import { currentUser } from "@clerk/nextjs/server"
 
 export const onAuthenticatedUser = async () => {
@@ -8,17 +8,31 @@ export const onAuthenticatedUser = async () => {
         const clerk = await currentUser()
         if(!clerk) return {status: 404}
 
-        // const user = await client.user.findUnique({
-        //     where: {
-        //         clerkId: clerk.id
-        //     },
-        //     select: {
-        //         id: true,
-        //         firstname: true,
-        //         lastname: true
-        //     }
-        // })
+        const user = await client.user.findUnique({
+            where: {
+                clerkId: clerk.id
+            },
+            select: {
+                id: true,
+                firstname: true,
+                lastname: true
+            }
+        })
+        if(user)
+        {
+            return {
+                status: 200,
+                id: user.id,
+                image: clerk.imageUrl,
+                username: `${user.firstname} ${user.lastname}`,
+            }
+        }
+        return {
+            status: 404,
+        }
     } catch (error) {
-        
+        return {
+            status:404,
+        }
     }
 }
